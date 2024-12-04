a27754311595faf8901ac4e149245907d34090af
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-04 13:34:39 +0600

    Add files via upload

---
+++ User_two_inpu t_.asm
@@ -0,0 +1,27 @@
+.model small
+.stack 100h
+.code
+
+main proc
+    mov ah,1
+    int 21h     ;first input
+    mov bl,al 
+    
+    mov ah,1
+    int 21h     ;second input
+    mov bh,al
+    
+    
+    mov ah,2
+    mov dl,bl   ;fist output
+    int 21h
+    
+    mov ah,2
+    mov dl,bh   ;second output
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
