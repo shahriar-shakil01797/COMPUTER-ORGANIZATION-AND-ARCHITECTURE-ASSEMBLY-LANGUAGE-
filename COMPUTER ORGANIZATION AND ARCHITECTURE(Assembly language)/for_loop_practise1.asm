32cfc6656396b556a20c879b733077ea75a44f2e
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-27 13:12:27 +0600

    Add files via upload

---
+++ COMPUTER ORGANIZATION AND ARCHITECTURE(Assembly language)/for_loop_practise1.asm
@@ -0,0 +1,38 @@
+.model small
+.stack 100h
+.code
+ 
+main proc
+    
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    
+    mov cl,'1'
+    
+    for:
+    cmp cl,bl
+    je exit:
+    
+    mov ah,2
+    mov dl,cl
+    int 21h
+    
+    inc cl
+    
+    jmp for:
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
+
+    
