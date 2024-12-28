681d11c2bef201bd0edd9cf67dd8bd40c058f915
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-28 17:08:54 +0600

    Add files via upload

---
+++ COMPUTER ORGANIZATION AND ARCHITECTURE(Assembly language)/Print A to Z using loop_small.asm
@@ -0,0 +1,22 @@
+.model small
+.stack 100h
+.code
+
+main proc
+    
+    mov cx,26
+    mov ah,2
+    mov dl,'a'
+    
+    top:
+    int 21h
+    inc dl
+    
+    loop top:
+    
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
