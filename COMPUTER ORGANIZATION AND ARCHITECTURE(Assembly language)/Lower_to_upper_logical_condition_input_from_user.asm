c7ad0b689dc037c3ba54349efcd951cee4187202
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-31 20:54:30 +0600

    Add files via upload

---
+++ COMPUTER ORGANIZATION AND ARCHITECTURE(Assembly language)/Lower_to_upper_logical_condition_input_from_user.asm
@@ -0,0 +1,47 @@
+.model small
+ .stack 100h
+ .data
+ x db "Enter lowercase alphabet = $" 
+ y db "uppercase alphabet = $"
+ .code
+ 
+ main proc
+    
+    mov ax,@data
+    mov ds,ax
+    
+    
+    mov ah,9
+    lea dx,x
+    int 21h
+    
+    
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    
+    AND bl,0dfh
+    
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13 
+    int 21h
+    
+    
+    mov ah,9
+    lea dx,y
+    int 21h
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+ end main
+ 
