1c334fa951dc09623d518fbfdc2ab363bdbfe5dc
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-25 11:00:05 +0600

    Add files via upload

---
+++ COMPUTER ORGANIZATION AND ARCHITECTURE(Assembly language)/ASCII_all_value.asm
@@ -0,0 +1,24 @@
+.model small
+.stack 100h
+.code
+
+main proc
+    
+    mov cx,256
+    
+    mov ah,2
+    mov dl,0
+    
+    loop:
+      int 21h
+      inc dl
+      dec cx
+      
+    JNZ loop
+    
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
